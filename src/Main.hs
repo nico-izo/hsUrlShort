9fb15c5851a3fffee324baf1491f634b8369bccd
Nicolay Izoderov <nico-izo@ya.ru>
2015-07-08 18:26:43 +0300

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes, TypeFamilies, EmptyDataDecls,
+             FlexibleContexts, GADTs, OverloadedStrings, ScopedTypeVariables,
+             MultiParamTypeClasses, GeneralizedNewtypeDeriving #-}
+
+module Main where
+import Data.Char (ord, chr)
+import Data.Time.Clock
+import Data.Text.Lazy as T (pack)
+import Database.Persist
+import Database.Persist.TH
+import qualified Database.Persist.Sqlite as Db
+import Text.Hamlet
+import Web.Scotty as S
+import Network.HTTP.Types
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Logger
+import Control.Monad.Trans.Resource
+import Control.Applicative (pure)
+
+idToHash :: Int -> [Char]
+idToHash i | i <= 0 = error "invalid id"
+idToHash i = let numberOfChars = length ['a'..'z']
+                 firstCharNum = ord 'a'
+           in map (chr.(+ firstCharNum).(`mod` numberOfChars))
+           $ reverse 
+           $ takeWhile (/= 0) 
+           $ iterate (`div` numberOfChars) i
+
+hashToId :: [Char] -> Int
+hashToId str | str == "" = error "empty string"
+hashToId str = let numberOfChars = length ['a'..'z']
+                   firstCharNum = ord 'a'
+                   arrOfExps = reverse $ take (length str) $ iterate (* numberOfChars) 1
+             in sum $ zipWith (*) arrOfExps $ map ((+ negate firstCharNum).(ord)) str
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+ShortUrl
+    origUrl String
+    deriving Show
+|]
+
+main :: IO ()
+main = do
+    runResourceT $ runStderrLoggingT $ Db.runSqlite "app.db" $ Db.runMigration migrateAll
+    runStderrLoggingT $ Db.withSqlitePool "app.db" 10 $ \pool -> liftIO $ scotty 3000 $ do
+        S.get "/" $ do
+            html $ renderHtml [shamlet|
+                <html>
+                    <head>
+                        <title>Tiny Url Shortener
+                    <body>
+                        <h1>NanoUrlShortener 0.0.1
+                        <form action=/ method=post>
+                            <input name=url type=text placeholder=Url to short>
+                            <input type=submit>
+            |]
+
+        post "/" $ do
+            (urlToShort :: String) <- param "url"
+            urlId <- liftIO $ flip Db.runSqlPersistMPool pool $ Db.insert $ (ShortUrl urlToShort)
+            html $ renderHtml [shamlet|
+                <html>
+                    <head>
+                        <title>Tiny Url Shortener
+                    <body>
+                        <h1>Get your shortened link
+                        <p>http://localhost:3000/#{idToHash (fromIntegral $ Db.fromSqlKey urlId)}
+            |]
+        S.get "/:urlHash" $ do
+            (urlHash :: String) <- param "urlHash"
+            unshortUrl <- liftIO $ flip Db.runSqlPersistMPool pool $ Db.get
+                        $ (Db.toSqlKey $ fromIntegral $ hashToId urlHash)
+            case unshortUrl of
+                Just (ShortUrl u) -> do
+                    status status302
+                    setHeader "location" (T.pack u)
+                Nothing -> do
+                    status notFound404
+                    html "<h1>404 Not Found</h1>"
+
+        notFound $ do
+            status notFound404
+            html "<h1>404 Not Found</h1>"
